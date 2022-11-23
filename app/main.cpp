21d9d113a80931eb276d261e13ffac607e4207ce
toby-schonrock <tobias@schonrocks.com>
2022-11-23 17:23:30 +0000

    RingBuffer working with implot

--- app/main.cpp
+++ app/main.cpp
@@ -1,25 +1,23 @@
 #include <array>
 #include <chrono>
 #include <cmath>
-#include <cstddef>
 #include <cstdio>
 #include <iostream>
 #include <numbers>
 #include <string>
 
-#include "Graph.hpp"
 #include "Matrix.hpp"
 #include "Point.hpp"
 #include "Polygon.hpp"
+#include "RingBuffer.hpp"
 #include "Vector2.hpp"
 #include "imgui-SFML.h"
 #include "imgui.h"
-#include "imgui_internal.h"
 #include "implot.h"
 
-float                vsScale = 0;
-extern unsigned char arial_ttf[]; // NOLINT
-extern const unsigned int  arial_ttf_len;
+float                     vsScale = 0;
+extern unsigned char      arial_ttf[]; // NOLINT
+extern const unsigned int arial_ttf_len;
 
 class SoftBody {
   public:
@@ -89,32 +87,30 @@ sf::Vector2f visualize(const Vec2& v) {
     return sf::Vector2f(static_cast<float>(v.x), static_cast<float>(v.y)) * vsScale;
 }
 
-void displayFps(double Vfps, double Sfps, sf::RenderWindow& window, const sf::Font& font,
-                Graph<double>& fps) {
+void displayFps(const RingBuffer<Vec2>& fps) {
     ImGui::Begin("FPS");
-    if (ImPlot::BeginPlot("fps", {-1.0F , -1.0F}, ImPlotFlags_NoInputs | ImPlotFlags_NoTitle)) { // NOLINT "Use of a signed integer operand with a binary bitwise operator" this is implots fault
-        ImPlot::SetupAxis(ImAxis_X1, nullptr, ImPlotAxisFlags_NoDecorations);
+    if (ImPlot::BeginPlot(
+            "fps", {-1.0F, -1.0F},
+            ImPlotFlags_NoInputs |
+                ImPlotFlags_NoTitle)) { // NOLINT "Use of a signed integer operand with a binary
+                                        // bitwise operator" this is implots fault
+        ImPlot::SetupAxis(ImAxis_X1, nullptr, ImPlotAxisFlags_NoDecorations); // setup axes
         ImPlot::SetupAxis(ImAxis_Y1, "visual");
-        ImPlot::SetupAxesLimits(0,160,0,100, ImGuiCond_Always);
-        ImPlot::SetupAxis(ImAxis_Y2, "simulation");
+        ImPlot::SetupAxesLimits(0, 160, 0, 100, ImGuiCond_Always);
+        ImPlot::SetupAxis(ImAxis_Y2, "simulation",
+                          ImPlotAxisFlags_Opposite | ImPlotAxisFlags_NoSideSwitch);
         ImPlot::SetupAxisScale(ImAxis_Y2, ImPlotScale_Log10);
-        ImPlot::SetupAxisLimits(ImAxis_Y2,1,100000);
+        ImPlot::SetupAxisLimits(ImAxis_Y2, 1, 100000);
 
-        // Implot::
-        ImPlot::PlotLine("visual", fps.vx.data(), static_cast<int>(fps.vx.size()));
+        ImPlot::PlotLine("visual", &fps.v[0].x, static_cast<int>(fps.v.size()), 1.0L, 0.0L,
+                         ImPlotLineFlags_None, static_cast<int>(fps.pos),
+                         sizeof(Vec2)); // plot graphs
         ImPlot::SetAxes(ImAxis_X1, ImAxis_Y2);
-        ImPlot::PlotLine("simulation", fps.vy.data(), static_cast<int>(fps.vy.size()), 1, ImPlotLineFlags_None, fps.vy.pos());
+        ImPlot::PlotLine("simulation", &fps.v[0].y, static_cast<int>(fps.v.size()), 1, 0.0L,
+                         ImPlotLineFlags_None, static_cast<int>(fps.pos), sizeof(Vec2));
         ImPlot::EndPlot();
     }
     ImGui::End();
-
-    sf::Text text;
-    text.setFont(font); // font is a sf::Font
-    text.setString(std::to_string(static_cast<int>(Vfps)) + " " +
-                   std::to_string(static_cast<int>(Sfps)));
-    text.setCharacterSize(24); // in pixels, not points!
-    text.setFillColor(sf::Color::Red);
-    window.draw(text);
 }
 
 void displaySimSettings(SoftBody& sb, float& gravity) {
@@ -136,37 +132,31 @@ void displaySimSettings(SoftBody& sb, float& gravity) {
 }
 
 int main() {
-    float gravity = 2;
-    Graph<double> fps(std::pair<std::string, std::string>("time", "fps"), 160);
-
     sf::VideoMode               desktop = sf::VideoMode::getDesktopMode();
     const Vector2<unsigned int> screen(desktop.width, desktop.height);
     vsScale = 25.0F / 512.0F * static_cast<float>(screen.x); // window scaling
 
-    sf::Font font;
-    if (!font.loadFromMemory(arial_ttf, arial_ttf_len)) { // NOLINT
-        std::cout << "Font file not found";
-        return (EXIT_FAILURE);
-    }
-
     sf::ContextSettings settings;
     settings.antialiasingLevel = 8;
 
-    sf::RenderWindow window(sf::VideoMode::getDesktopMode(), "Simulation",
-                            sf::Style::Fullscreen, settings); //, sf::Style::Default);
+    sf::RenderWindow window(sf::VideoMode::getDesktopMode(), "Simulation", sf::Style::Fullscreen,
+                            settings); //, sf::Style::Default);
     ImGui::SFML::Init(window);
     ImPlot::CreateContext();
 
     SoftBody sb(Vec2I(25, 25), 0.2F, Vec2(3, 0), 10000, 100);
+    float            gravity = 2;
 
     std::vector<Polygon> polys;
     polys.push_back(Polygon::Square(Vec2(6, 10), -0.75));
     polys.push_back(Polygon::Square(Vec2(14, 10), 0.75));
     polys.push_back(Polygon::Triangle(Vec2(100, 100)));
 
-    std::chrono::_V2::system_clock::time_point last = std::chrono::high_resolution_clock::now();
+    RingBuffer<Vec2> fps(160);
     double                                     Vfps = 0;
+    double                                     Sfps = 0;
 
+    std::chrono::_V2::system_clock::time_point last = std::chrono::high_resolution_clock::now(); // setting time of previous frame to be now
     sf::Clock
         deltaClock; // for imgui - read https://eliasdaler.github.io/using-imgui-with-sfml-pt1/
     while (window.isOpen()) {
@@ -181,10 +171,8 @@ int main() {
         }
 
         ImGui::SFML::Update(window, deltaClock.restart());
-        displaySimSettings(sb, gravity);
 
-        int simFrames = 0;
-
+        int                      simFrames   = 0;
         std::chrono::nanoseconds sinceVFrame = std::chrono::high_resolution_clock::now() - start;
         while ((sinceVFrame.count() < 10'000'000)) { // TODO: min max avg frames test
             ++simFrames;
@@ -199,26 +187,27 @@ int main() {
         }
 
         // draw
-        double Sfps = 1e9 * simFrames / static_cast<double>(sinceVFrame.count());
-        // double Sfps = simFrames;
 
         window.clear();
-        displayFps(Vfps, Sfps, window, font, fps);
-        fps.add(Vfps, Sfps);
+
+        //imgui windows
+        displaySimSettings(sb, gravity);
+        displayFps(fps);
 
         sb.draw(window);
         for (Polygon& poly: polys) poly.draw(window);
 
-        ImGui::SFML::Render(window); // end(has been moved) and draw
+        ImGui::SFML::Render(window);
         window.display();
 
         sinceVFrame = std::chrono::high_resolution_clock::now() - start;
-        // std::cout << sinceVFrame.count() << "ns\n";
+        Sfps = 1e9 * simFrames / static_cast<double>(sinceVFrame.count());
         Vfps = 1e9 / static_cast<double>(sinceVFrame.count());
+        fps.add({Vfps, Sfps});
     }
 
     ImPlot::DestroyContext();
     ImGui::SFML::Shutdown();
-    
+
     return 0;
 }
